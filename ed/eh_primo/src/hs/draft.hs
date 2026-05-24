719cfca4e8743d30f5b41663cbeb116211bffcbd
warrley <warleeyfarias@gmail.com>
2026-05-24 14:33:34 -0300

    eh_primo haskell

--- ed/eh_primo/src/hs/draft.hs
+++ ed/eh_primo/src/hs/draft.hs
@@ -1,10 +1,9 @@
 main :: IO ()
-main = do 
-  input <- getLine 
-  let n = read input :: Int 
-  putStrLn (if prime n then "true" else "false")
-
 prime :: Int -> Bool
-prime n 
-  | n <= 1 = False
-  | otherwise = length [x | x <- [2..(n-1)], n `mod` x == 0] == 0
+prime n = length [x | x <- [1..n], mod n x == 0] == 2
+
+main = do 
+  input <- getLine
+  if prime (read input) 
+    then putStrLn "true"
+  else putStrLn "false"
